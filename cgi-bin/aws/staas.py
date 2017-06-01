9167b608a884d03ec3760d13b88bcb4bf17212c1
root <root@dns.krak.org>
2017-06-01 17:25:01 +0530

    aws started

---
+++ cgi-bin/aws/staas.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python2
+import cgi,commands
+server_ip = "0.0.0.0"
+
+print "Content-type:text/html"
+print ""
+
+data = cgi.FieldStorage()
+
+dname = data.getvalue("dname")
+dsize = data.getvalue("dsize")
+uname = data.getvalue("uname")
+
+instance_id = "i-05327d8531056e3e9"
+instance_type = "t2.micro"
+availability_zone = "us-west-2c"
+
+#print "sudo aws ec2 create-volume --size {} --region us-east-2 --availability-zone {} --volume-type gp2".format(dsize,availability_zone)+"\n"
+x=commands.getoutput("sudo aws ec2 create-volume --size {} --region us-west-2 --availability-zone us-west-2c --volume-type gp2".format(dsize))
+vid = (x.split(","))[3].split(":")[1]
+print "<pre>"
+print commands.getoutput("sudo aws ec2 attach-volume --volume-id {} --instance-id {} --region us-west-2 --device /dev/sdf".format(vid,instance_id))
+
+print "</pre>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print dname
