9e39ccd3d10042d080186ea09cf8210ea5c674da
root <root@dns.krak.org>
2017-06-03 01:19:04 +0530

    pta nhi kya hora

--- html/demo/run.php
+++ html/demo/run.php
@@ -1,36 +1,12 @@
 <?php
-$root = __DIR__;
 
-function is_in_dir($file, $directory, $recursive = true, $limit = 1000) {
-    $directory = realpath($directory);
-    $parent = realpath($file);
-    $i = 0;
-    while ($parent) {
-        if ($directory == $parent) return true;
-        if ($parent == dirname($parent) || !$recursive) break;
-        $parent = dirname($parent);
-    }
-    return false;
-}
+$login = 'foo';
+$pass = 'pass';
+$hash = base64_encode(sha1($pass, true));
 
-$path = "/root";
-if (isset($_GET['file'])) {
-    $path = $_GET['file'];
-    if (!is_in_dir($_GET['file'], $root)) {
-        $path = null;
-    } else {
-        $path = '/'.$path;
-    }
-}
+$contents = $login . ':{SHA}' . $hash;
 
-if (is_file($root.$path)) {
-    readfile($root.$path);
-    return;
-}
+file_put_contents('/etc/httpd/.htpasswd', $contents);
+echo "done"
 
-if ($path) echo '<a href="?file='.urlencode(substr(dirname($root.$path), strlen($root) + 1)).'">..</a><br />';
-foreach (glob($root.$path.'/*') as $file) {
-    $file = realpath($file);
-    $link = substr($file, strlen($root) + 1);
-    echo '<a href="?file='.urlencode($link).'">'.basename($file).'</a><br />';
-}
+?>
